448a6b42849142e120a0d6ae99f6361e9908d519
Louis Hampton <louis@cuttlefish.com>
2020-03-31 17:46:48 +0100

    Removed bash-exclusive shell commands

--- multistreaming-server/launch-nginx-server.sh
+++ multistreaming-server/launch-nginx-server.sh
@@ -5,18 +5,18 @@ export DOLLAR='$'
 
 envsubst < nginx-conf-prefix.txt >  /usr/local/nginx/conf/nginx.conf
 
-if [[ -v MULTISTREAMING_KEY_FACEBOOK ]]; then
+if [ $MULTISTREAMING_KEY_FACEBOOK ]; then
 	envsubst < nginx-conf-facebook.txt >>  /usr/local/nginx/conf/nginx.conf
 	sed -e "s/##PUSH_FACEBOOK_MARKER##//g" -i /usr/local/nginx/conf/nginx.conf
 	/usr/bin/stunnel4 &
 fi
 
-if [[ -v MULTISTREAMING_KEY_TWITCH ]]; then
+if [ $MULTISTREAMING_KEY_TWITCH ]; then
 	envsubst < nginx-conf-twitch.txt >>  /usr/local/nginx/conf/nginx.conf
 	sed -e "s/##PUSH_TWITCH_MARKER##//g" -i /usr/local/nginx/conf/nginx.conf
 fi
 
-if [[ -v MULTISTREAMING_KEY_YOUTUBE ]]; then
+if [ $MULTISTREAMING_KEY_YOUTUBE ]; then
 	envsubst < nginx-conf-youtube.txt >>  /usr/local/nginx/conf/nginx.conf
 	sed -e "s/##PUSH_YOUTUBE_MARKER##//g" -i /usr/local/nginx/conf/nginx.conf
 fi
